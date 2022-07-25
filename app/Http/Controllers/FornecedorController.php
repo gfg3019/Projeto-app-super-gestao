5c1fb5aa7c7e8668b6306411756f7a51ca857fb0
gfg3019 <genissongomes@gmail.com>
2022-07-25 15:55:33 -0300

    terminando estudos basicos so o blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,9 +8,32 @@ class FornecedorController extends Controller
 {
     public function index(){
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.000/000-00'],
-            2 => ['nome' => 'Fornecedor 2', 'status' => 'S']
+            0 =>
+            [
+                'nome' => 'Fornecedor 1',
+                'status' => 'N',
+                'cnpj' => '1231321321231',
+                'ddd' => '21',//Rio de Janeiro
+                'telefone' => '0000-0000'
+            ],
+            1 =>
+            [
+                'nome' => 'Fornecedor 2',
+                'status' => 'S',
+                'ddd' => '11',//São Paulo
+                'telefone' => '1111-1111'
+            ],
+
+            2 =>
+            [
+                'nome' => 'Fornecedor 3',
+                'status' => 'S',
+                'cnpj' => '984984989894',//Fortaleza
+                'ddd' => '85',
+                'telefone' => '0000-0000'
+            ]
         ];
+
         return view('app.fornecedores.index', compact('fornecedores'));
         //return view('app.fornecedores.index');
     }
